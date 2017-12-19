79d94efda25d756352baa7743d2fe83422fbeab8
Ryland Bowles <ryland.bowles@comcast.net>
2017-12-19 02:45:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // write your code below!
 var name = "Susan"
 var height = 39
-var message = ` is ${height} inches talll`
+var message = `${name} is ${height} inches talll`
 
 module.exports = {name, height, message}
